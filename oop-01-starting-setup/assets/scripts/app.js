ea97bf78c8cf13dc1456455f46227f086ae66091
Vahram Grigoryan <ramgrigoryan@gmail.com>
2022-03-07 13:28:47 +0400

    using Class for product objects

--- oop-01-starting-setup/assets/scripts/app.js
+++ oop-01-starting-setup/assets/scripts/app.js
@@ -1,19 +1,20 @@
+class ProductList{
+    title="DEFAULT";
+    imageUrl;
+    price;
+    description;
+    constructor(title,imageUrl,price,description){
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.price = price;
+        this.description = description;
+    }
+}
+
 const productList = {
   products: [
-    {
-      title: "Butter",
-      imageUrl:
-        "https://images.unsplash.com/photo-1589985270826-4b7bb135bc9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-      price: 10.69,
-      description: "Finnish natural butter",
-    },
-    {
-      title: "Olive oil",
-      imageUrl:
-        "https://images.unsplash.com/photo-1474979266404-7eaacbcd87c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=418&q=80",
-      price: 5.99,
-      description: "Greek olive oil",
-    },
+    new ProductList("Butter","https://images.unsplash.com/photo-1589985270826-4b7bb135bc9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",10.59,"Finnish natural butter"),
+    new ProductList("Olive oil","https://images.unsplash.com/photo-1474979266404-7eaacbcd87c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=418&q=80",5.99,"Greek olive oil"),
   ],
   renderItems() {
     const renderedList = document.getElementById("app");
